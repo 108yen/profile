d5bf42c24a75edfda41f56a0e58c0e2b6d250143
108yen <kazuking.1911@gmail.com>
2024-11-11 19:43:05 +0900

    refactor: refactoring of content

---
+++ src/constant/contents.ts
@@ -0,0 +1,90 @@
+export const YAMADA_UI = {
+  ariaLabel: "Yamada UI Link",
+  href: "https://github.com/yamada-ui/yamada-ui",
+  title: "Yamada UI",
+}
+
+export const CONTRIBUTING = [
+  {
+    ariaLabel: "recharts Link",
+    href: "https://github.com/recharts/recharts",
+    title: "recharts",
+  },
+]
+
+export const SKILLS = [
+  "Typescript",
+  "React",
+  "Next.js",
+  "Web Accessibility",
+  "Flutter",
+  "Google Cloud",
+  "Vercel",
+  "Github Actions",
+]
+
+export const PRODUCTS = [
+  {
+    ariaLabel: "Twitch clip ranking Link",
+    href: "https://www.twitchclipsranking.com/",
+    title: "Twitch clip ranking",
+  },
+
+  {
+    ariaLabel: "Copipe Link",
+    href: "https://www.netcopipe.com/",
+    title: "copipe",
+  },
+
+  {
+    ariaLabel: "Changeset-release Link",
+    href: "https://github.com/108yen/changeset-release",
+    title: "changeset-release",
+  },
+]
+
+export const CERTIFICATIONS = [
+  {
+    ariaLabel: "CompTIA Security+ Link",
+    expired: true,
+    href: "https://www.credly.com/badges/390af6d6-0529-4c1a-b08d-db17d5c3db2f",
+    title: "CompTIA Security+ certification",
+  },
+  { title: "情報処理安全確保支援士" },
+  {
+    ariaLabel: "AWS SAA Link",
+    expired: true,
+    href: "https://www.credly.com/badges/34605602-4be2-47de-8ffe-a85401649b31",
+    title: "AWS Certified Solutions Architect - Associate",
+  },
+  {
+    ariaLabel: "Associate Cloud Engineer Link",
+    href: "https://www.credly.com/badges/13640f30-1c62-4df2-b192-c904ff370f25",
+    title: "Associate Cloud Engineer",
+  },
+  {
+    ariaLabel: "Professional Cloud Architects Link",
+    href: "https://www.credly.com/badges/0f7513ab-4fa4-4c2e-96c7-6d819c50664b",
+    title: "Professional Cloud Architects",
+  },
+  {
+    ariaLabel: "Professional Cloud Developer Link",
+    href: "https://www.credly.com/badges/11953a8e-add0-41dd-99fe-04572dc5eeb4",
+    title: "Professional Cloud Developer",
+  },
+  {
+    ariaLabel: "Professional Cloud DevOps Engineer Link",
+    href: "https://www.credly.com/badges/3319c4ce-d5fb-43f8-81d7-c19eb0e87fa4",
+    title: "Professional Cloud DevOps Engineer",
+  },
+  {
+    ariaLabel: "Professional Cloud Security Engineer Link",
+    href: "https://www.credly.com/badges/1627bd31-8407-4014-912d-dc501a2ce3c2",
+    title: "Professional Cloud Security Engineer",
+  },
+  {
+    ariaLabel: "Professional Cloud Network Engineer Link",
+    href: "https://www.credly.com/badges/f1cf6b62-c7f7-47c8-a554-b30261fde3f2",
+    title: "Professional Cloud Network Engineer",
+  },
+]
