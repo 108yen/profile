30fe9073cbc31e06c7bcf8d2266d91db00d25fa4
108yen <kazuking.1911@gmail.com>
2025-05-25 16:27:04 +0900

    chore: sprite test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,123 @@
+"use client"
+
+import { useEffect, useRef } from "react"
+import {
+  PerspectiveCamera,
+  Scene,
+  Sprite,
+  SpriteMaterial,
+  WebGLRenderer,
+} from "three"
+
+export default function Page() {
+  const ref = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    const canvas = ref.current
+    if (!canvas) return
+
+    const scene = new Scene()
+    const camera = new PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000,
+    )
+    camera.position.x = 0
+    camera.position.y = 0
+    camera.position.z = 150
+
+    const renderer = new WebGLRenderer({ canvas })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    renderer.setClearColor(0x000000)
+
+    scene.add(camera)
+    createSprites()
+
+    requestAnimationFrame(render)
+    render()
+
+    function createSprites() {
+      const material = new SpriteMaterial()
+
+      for (let x = -5; x < 5; x++) {
+        for (let y = -5; y < 5; y++) {
+          const sprite = new Sprite(material)
+          sprite.position.set(x * 10, y * 10, 0)
+          scene.add(sprite)
+        }
+      }
+    }
+
+    function render() {
+      requestAnimationFrame(render)
+      renderer.render(scene, camera)
+    }
+  }, [])
+
+  return <canvas ref={ref} />
+}
+
+//     // once everything is loaded, we run our Three.js stuff.
+//     function init() {
+
+//         var stats = initStats();
+
+//         // create a scene, that will hold all our elements such as objects, cameras and lights.
+//         var scene = new THREE.Scene();
+
+//         // create a camera, which defines where we're looking at.
+//         var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+//         // create a render and set the size
+// //        var canvasRenderer = new THREE.CanvasRenderer();
+//         var canvasRenderer = new THREE.WebGLRenderer();
+//         canvasRenderer.setClearColor(new THREE.Color(0x000000));
+//         canvasRenderer.setSize(window.innerWidth, window.innerHeight);
+
+//         // position and point the camera to the center of the scene
+//         camera.position.x = 0;
+//         camera.position.y = 0;
+//         camera.position.z = 150;
+
+//         // add the output of the renderer to the html element
+//         document.getElementById("WebGL-output").appendChild(canvasRenderer.domElement);
+
+//         createSprites();
+//         render();
+
+//         function createSprites() {
+//             var material = new THREE.SpriteMaterial();
+
+//             for (var x = -5; x < 5; x++) {
+//                 for (var y = -5; y < 5; y++) {
+//                     var sprite = new THREE.Sprite(material);
+//                     sprite.position.set(x * 10, y * 10, 0);
+//                     scene.add(sprite);
+//                 }
+//             }
+//         }
+
+//         function render() {
+//             stats.update();
+
+//             requestAnimationFrame(render);
+//             canvasRenderer.render(scene, camera);
+//         }
+
+//         function initStats() {
+
+//             var stats = new Stats();
+//             stats.setMode(0); // 0: fps, 1: ms
+
+//             // Align top-left
+//             stats.domElement.style.position = 'absolute';
+//             stats.domElement.style.left = '0px';
+//             stats.domElement.style.top = '0px';
+
+//             document.getElementById("Stats-output").appendChild(stats.domElement);
+
+//             return stats;
+//         }
+//     }
+//     window.onload = init;
